4bd308b57d56095dc723b756adddd76667e8833f
saher amr <saher2110197@miuegypt.edu.eg>
2025-07-27 18:04:43 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+fn concatenate_string(string1: &str, string2: &str) -> String {
+    
+    format!("{string1}{string2}") 
+}
 fn main() {
-    println!("Hello, world!");
+    
+    let string1 = String::from("Saher ");
+    let string2 = String::from("Amr");
+
+    
+    let result = concatenate_string(&string1, &string2);
+    println!("String 1: {string1}");
+    println!("String 2: {string2}");
+    
+    println!("Concatenated string: {result}");
 }
